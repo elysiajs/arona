b07635aa01b86c3ab9dcb5b969f1aa8ea7a29d10
saltyaom <saltyaom@gmail.com>
2025-10-16 10:38:30 +0700

    :tada: feat: add reindex database

--- src/index.ts
+++ src/index.ts
@@ -101,7 +101,7 @@ const app = new Elysia()
 			})
 		}
 	)
-	.listen(process.env.PORT ?? 80)
+	.listen(process.env.PORT ?? 3000)
 
 console.log(
 	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
