c5ca59869f3e37f745ab146929e6a487b536f38e
saltyaom <saltyaom@gmail.com>
2026-02-13 19:24:44 +0700

    :tada: feat: embedding cache error

--- src/libs/redis.ts
+++ src/libs/redis.ts
@@ -1,36 +1,43 @@
 import { Redis } from 'ioredis'
+import { log } from './log'
 
 const REDIS_URL = process.env.REDIS_URL
 if (!REDIS_URL) throw new Error('REDIS_URL is not defined')
 
 export const redis = new Redis(REDIS_URL)
 
-redis
-	.connect()
-	.then((x) => {
-		redis
-			.call(
-				'FT.CREATE',
-				'idx:cache',
-				'ON',
-				'HASH',
-				'PREFIX',
-				'1',
-				'cache:',
-				'SCHEMA',
-				'embedding',
-				'VECTOR',
-				'HNSW',
-				'6',
-				'TYPE',
-				'FLOAT32',
-				'DIM',
-				'1536',
-				'DISTANCE_METRIC',
-				'COSINE',
-				'response',
-				'TEXT'
-			)
-			.catch(() => {})
-	})
-	.catch(() => {})
+redis.once('connect', () => {
+	redis
+		.call(
+			'FT.CREATE',
+			'idx:cache',
+			'ON',
+			'HASH',
+			'PREFIX',
+			'1',
+			'cache:',
+			'SCHEMA',
+			'embedding',
+			'VECTOR',
+			'HNSW',
+			'6',
+			'TYPE',
+			'FLOAT32',
+			'DIM',
+			'1536',
+			'DISTANCE_METRIC',
+			'COSINE',
+			'response',
+			'TEXT'
+		)
+		.then(() => {
+			log('Redis FT.CREATE index created')
+		})
+		.catch((error) => {
+			if (error.message.includes('Index already exists')) return
+
+			log('Redis FT.CREATE error occurred.', error)
+		})
+})
+
+redis.connect().catch(() => {})
